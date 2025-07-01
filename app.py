84546c51be1098dd804189ece26c0bf22beda801
BothLotus <hiramgtorres82@gmail.com>
2025-07-01 18:57:59 -0400

    added some python

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
-
+from flask import Flask, render_template, request, redirect, flash, url_for, session
+import requests
 app = Flask(__name__)
 
-api_key = "api-key"
+API_KEY = '42cde0c47c9e6b1e75515d281cc65587'
 
+api_url = 'https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API_KEY}'
 @app.route('/', methods=['GET','POST'])
 def dashboard():
     if request.method == "POST":
-        city = request.form['city']
-        session['city'] = city
+        zip_code = request.form['zip_code']
+        country_code = request.form['country_code']
+        session['zip_code'] = zip_code
+        session['country_code'] = country_code
         return render_template('dashboard.html')
+    return render_template('results.html')
     
-    response = requests.get(api_key)
+    response = requests.get(api_url)
+
+@app.route('/results')
+def results():
+    zip_code = session.get('zip_code')
+    country_code = session.get('country_code')
+    if not zip_code or not country_code:
+        return redirect(url_for('dashboard'))
+    response = requests.get(api_url)
+
+    weather_data = response.json()
+    return render_template('result.html', data=weather_data, city=f"{zip_code}, {country_code}")
