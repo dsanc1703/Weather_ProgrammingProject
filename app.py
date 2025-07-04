0415059fee4fd6511547e5599688dbdd75a324e5
Danniell Sanchez-Palacios <sanchezdanniell@gmail.com>
2025-07-03 20:39:45 -0400

    login verification

--- app.py
+++ app.py
@@ -6,18 +6,23 @@ API_KEY = '42cde0c47c9e6b1e75515d281cc65587'
 
 #api_url = 'https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API_KEY}'
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/zipcode', methods=['GET','POST'])
+users = []
+
+
+@app.route('/zipcode', methods=['GET', 'POST'])
 def zipcode():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
     if request.method == "POST":
         zip_code = request.form['zip_code']
         country_code = request.form['country_code']
         session['zip_code'] = zip_code
         session['country_code'] = country_code
         return redirect(url_for('results'))
+
     return render_template('zipcode.html')
-    
-    #response = requests.get(api_url)
+
 
 @app.route('/results')
 def results():
@@ -25,34 +30,40 @@ def results():
     country_code = session.get('country_code')
     if not zip_code or not country_code:
         return redirect(url_for('dashboard'))
-    response = requests.get('https://api.openweathermap.org/data/2.5/weather?zip='+zip_code+','+country_code+'&appid='+API_KEY)
 
+    response = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={API_KEY}'
+    )
     weather_data = response.json()
     return render_template('results.html', data=weather_data, zipcode=zip_code, countrycode=country_code)
 
-@app.route('/signup', methods=['GET', 'POST']) 
-def signup():    
-  if request.method == 'POST':   
-    username = request.form.get('username')
-    password = request.form.get('password')
-    users.append({'username':username, 'password':password})  
-    return redirect(url_for('login'))
-    #return '<h2>Thanks for signing up, ' + username + '!</h2>' #a confirmation message for the user
-  return render_template('signup.html')  #When user enters /signup, this line loads the html for signup
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        users.append({'username': username, 'password': password})
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'POST':
-    username = request.form.get('username')
-    password = request.form.get('password')
-    for user in users:
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        for user in users:
             if user['username'] == username and user['password'] == password:
+                session['logged_in'] = True
                 return redirect(url_for('dashboard'))
-              
-    return "<h1>There is no account matching. Try again</h1>"
+        flash("Invalid login.")
+    return render_template('login.html')
 
-  return render_template('login.html')
 
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
-  return render_template('dashboard.html')
+    if session.get('logged_in'):
+        return render_template('dashboard.html')
+    return redirect(url_for('login'))
